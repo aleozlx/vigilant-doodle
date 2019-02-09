f9bdb249479220f472b5eae80cddc6413f21de15
Alex Yang <zy5f9@mail.missouri.edu>
2019-02-09 02:43:20 -0600

    ..

--- app/main.py
+++ app/main.py
@@ -8,10 +8,6 @@ err = lambda d: (json.dumps(dict(status='err', msg=d)), 500)
 ok = lambda d: json.dumps(dict(status='ok', data=d))
 pending = lambda d: json.dumps(dict(status='pending', msg=d))
 
-@app.route("/")
-def hello():
-    return "Hello World from Flask"
-
 @app.route('/version')
 def version():
     return ok('v0.1.0')
