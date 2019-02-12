593c8411011af078c7d1a0c71567b61906435e40
Alex Yang <zy5f9@mail.missouri.edu>
2019-02-12 13:00:17 -0600

    round trip! and we desperately need storage now

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,11 @@
+# pylint: disable=import-error,no-name-in-module
 import os, sys
 from flask import Flask, request, send_file
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 import json
+from tasks import task_hello
+import time
 
 err = lambda d: (json.dumps(dict(status='err', msg=d)), 500)
 ok = lambda d: json.dumps(dict(status='ok', data=d))
@@ -12,3 +15,20 @@ pending = lambda d: json.dumps(dict(status='pending', msg=d))
 def version():
     return ok('v0.1.0')
 
+def test_redis_jobq():
+    task = task_hello.delay()
+    task_id = task.id
+    r = task_hello.AsyncResult(task_id)
+    while r.state not in ['SUCCESS', 'FAILURE']:
+        time.sleep(2)
+    return ok(dict(
+        task_id = task_id,
+        state = r.state
+    ))
+
+@app.route('/t/<test_fn>', methods=['POST'])
+def test(test_fn):
+    if test_fn.startswith('test_'):
+        return globals()[test_fn]()
+    else:
+        return err('test case not found')
